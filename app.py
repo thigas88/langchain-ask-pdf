4beeb0b46183bc6d1fb06f2d300792381219da5e
Thiago Mendes Borges <thigas88@gmail.com>
2024-12-03 20:47:17 -0300

    Ajustando variáveis de ambiente.

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
-
+import os
 
 def main():
     load_dotenv()
@@ -42,9 +42,8 @@ def main():
       if user_question:
         docs = knowledge_base.similarity_search(user_question)
         
-        llm = OpenAI(
-apiKey=OPENAI_API_KEY,
-baseUrl=OPENAI_BASE_URL)
+        llm = OpenAI(apiKey=os.env.OPENAI_API_KEY,
+                     baseUrl=os.env.OPENAI_BASE_URL)
         chain = load_qa_chain(llm, chain_type="stuff")
         with get_openai_callback() as cb:
           response = chain.run(input_documents=docs, question=user_question)
